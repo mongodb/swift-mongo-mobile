ddba8bf7357471c5d0fc4a6ecfa27cffcf0c193a
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2018-06-13 16:47:10 -0400

    Fix indentation and various minor changes

--- Tests/MongoMobileTests/MongoMobileTests.swift
+++ Tests/MongoMobileTests/MongoMobileTests.swift
@@ -9,11 +9,11 @@ final class MongoMobileTests: XCTestCase {
     }
 
     func testMongoMobile() throws {
-      let client = MongoMobile.create(settings: MongoClientSettings [ dbPath: "test-path" ])
-      let coll = try client.db("test").collection("foo")
-      let insertResult = try coll.insertOne([ "test": 42 ])
-      let findResult = try coll.find([ "_id": insertResult!.insertedId ])
-      let docs = Array(findResult)
-      XCTAssertEqual(docs[0]["test"] as? Int, 42)
+        let client = MongoMobile.create(settings: MongoClientSettings(dbPath: "test-path"))
+        let coll = try client.db("test").collection("foo")
+        let insertResult = try coll.insertOne([ "test": 42 ])
+        let findResult = try coll.find([ "_id": insertResult!.insertedId ])
+        let docs = Array(findResult)
+        XCTAssertEqual(docs[0]["test"] as? Int, 42)
     }
 }
