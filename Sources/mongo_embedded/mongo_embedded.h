b351433e728f136c1e2473a208a0da8e6237ce50
Matt Broadstone <mbroadst@gmail.com>
2018-06-13 14:48:31 -0400

    Update include search path to use `mongo/embedded-v1` prefix

--- Sources/mongo_embedded/mongo_embedded.h
+++ Sources/mongo_embedded/mongo_embedded.h
@@ -1,2 +1,2 @@
-#include <mongo/embedded-v1/mongo/embedded/capi.h>
-#include <mongo/embedded-v1/mongo/embedded/mongoc_client.h>
+#include <mongo/embedded/capi.h>
+#include <mongo/embedded/mongoc_client.h>
