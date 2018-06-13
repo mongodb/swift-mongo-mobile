e7f9352b16eb6368e92d6ac5a2512651058fb365
Matt Broadstone <mbroadst@gmail.com>
2018-06-13 16:06:41 -0400

    Add a default SwiftPM Package.swift definition

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:4.0
+import PackageDescription
+let package = Package(
+    name: "MongoMobile",
+    products: [
+        .library(name: "MongoMobile", targets: ["MongoMobile"])
+    ],
+    targets: [
+        .target(name: "MongoMobile")
+    ]
+)
