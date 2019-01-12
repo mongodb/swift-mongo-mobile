919d9740d77951908ac0429428bca038ddedffdb
Matt Broadstone <mbroadst@gmail.com>
2019-01-12 11:55:25 -0500

    Update test to pass a cocoapods linting

--- Tests/MongoMobileTests/MongoMobileTests.swift
+++ Tests/MongoMobileTests/MongoMobileTests.swift
@@ -2,14 +2,34 @@
 import XCTest
 
 final class MongoMobileTests: XCTestCase {
-    static var allTests: [(String, (ClientTests) -> () throws -> Void)] {
+    static var allTests: [(String, (MongoMobileTests) -> () throws -> Void)] {
         return [
             ("testMongoMobile", testMongoMobile)
         ]
     }
 
     func testMongoMobile() throws {
-        let client = MongoMobile.create(settings: MongoClientSettings(dbPath: "test-path"))
+        try MongoMobile.initialize()
+        defer {
+            try? MongoMobile.close()
+        }
+
+        // setup database
+        let documentPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        let databasePath = documentPath.appendingPathComponent("test-mongo-mobile")
+
+        do {
+            try FileManager.default.removeItem(at: databasePath)
+        } catch {}
+
+        do {
+            try FileManager.default.createDirectory(at: databasePath, withIntermediateDirectories: false)
+        } catch {}
+
+        let settings = MongoClientSettings(dbPath: databasePath.path)
+        let client = try MongoMobile.create(settings)
+
+        // execute the test
         let coll = try client.db("test").collection("foo")
         let insertResult = try coll.insertOne([ "test": 42 ])
         let findResult = try coll.find([ "_id": insertResult!.insertedId ])
